672be49aa8a0777630ab5ead7dcb9320f6c47f97
Pedro Nauck <pedronauck@gmail.com>
2018-03-24 15:43:09 -0300

    feat(docz-core): set babel on the fly

--- packages/playgrodd-core/src/compiler/create-config.ts
+++ packages/playgrodd-core/src/compiler/create-config.ts
@@ -1,45 +1,39 @@
-import * as fs from 'fs'
 import * as path from 'path'
-import * as findup from 'find-up'
 import { Loader, Configuration } from 'webpack'
 import * as webpack from 'webpack'
 import * as HtmlWebpackPlugin from 'html-webpack-plugin'
+import * as merge from 'deepmerge'
 
 import { IEntryObj } from './files-parser'
+import { loadConfig } from '../utils/load-config'
 import * as paths from '../config/paths'
 
-const babelLoader = (babelrc: string | null): Loader => ({
-  loader: require.resolve('babel-loader'),
-  options: babelrc
-    ? JSON.parse(babelrc)
-    : {
-        babelrc: false,
-        cacheDirectory: true,
-        presets: [
-          require.resolve('@babel/preset-env'),
-          require.resolve('@babel/preset-react'),
-        ],
-      },
-})
+const babelLoader = (): Loader => {
+  const babelrc = loadConfig('babel', null)
+  const options = merge(babelrc, {
+    babelrc: false,
+    cacheDirectory: true,
+    presets: [
+      require.resolve('@babel/preset-env'),
+      require.resolve('@babel/preset-react'),
+    ],
+    plugins: [require.resolve('react-hot-loader/babel')],
+  })
+
+  return {
+    options,
+    loader: require.resolve('babel-loader'),
+  }
+}
 
 export const createConfig = async (
   entries: IEntryObj[]
 ): Promise<Configuration> => {
-  const babelrcPath = findup.sync(['.babelrc', 'babelrc.js'])
-  const babelrc = babelrcPath ? fs.readFileSync(babelrcPath, 'utf-8') : null
-
   return {
     mode: 'development',
     context: paths.ROOT,
     devtool: '#source-map',
-    entry: [
-      require.resolve('babel-polyfill'),
-      `${require.resolve(
-        'webpack-hot-middleware/client'
-      )}?path=/__webpack_hmr&timeout=20000'`,
-      ...entries.map(entry => entry.filepath),
-      paths.INDEX_JS,
-    ],
+    entry: [require.resolve('babel-polyfill'), paths.INDEX_JS],
     output: {
       pathinfo: true,
       path: paths.DIST,
@@ -56,7 +50,7 @@ export const createConfig = async (
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           include: [paths.ROOT],
-          use: babelLoader(babelrc),
+          use: babelLoader(),
         },
       ],
     },
@@ -66,6 +60,10 @@ export const createConfig = async (
       alias: {
         src: path.join(paths.ROOT, 'src'),
       },
+    },
+    devServer: {
+      contentBase: paths.DIST,
+      hot: true,
     },
     plugins: [
       new webpack.NamedModulesPlugin(),
