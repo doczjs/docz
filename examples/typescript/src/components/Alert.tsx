9052d0f110c80a7ebeb1997110beb87e56ffb236
Pedro Nauck <pedronauck@gmail.com>
2018-05-28 16:12:55 -0300

    feat(docz-example-typescript): add new example

---
+++ examples/typescript/src/components/Alert.tsx
@@ -0,0 +1,34 @@
+import React, { SFC } from 'react'
+import styled from 'react-emotion'
+
+export type Kind = 'info' | 'positive' | 'negative' | 'warning'
+export type KindMap = Record<Kind, string>
+
+const kinds: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+}
+
+export interface AlertProps {
+  /**
+   * Set this to change alert kind
+   * @default info
+   */
+  kind: 'info' | 'positive' | 'negative' | 'warning'
+}
+
+const AlertStyled = styled<AlertProps, 'div'>('div')`
+  padding: 15px 20px;
+  background: white;
+  border-radius: 3px;
+  color: white;
+  background: ${({ kind = 'info' }) => kinds[kind]};
+`
+
+const Alert: SFC<AlertProps> = ({ kind, ...props }) => (
+  <AlertStyled {...props} kind={kind} />
+)
+
+export default Alert
