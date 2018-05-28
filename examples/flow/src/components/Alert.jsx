27002d870afa50d3d790297b9cfae62dacebea2c
Pedro Nauck <pedronauck@gmail.com>
2018-05-28 20:11:29 -0300

    feat(docz-example-flow): add example folder

---
+++ examples/flow/src/components/Alert.jsx
@@ -0,0 +1,35 @@
+// @flow
+
+import React, { Fragment } from 'react'
+import styled from 'react-emotion'
+import t from 'prop-types'
+
+const kinds = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+}
+
+type Kind = 'info' | 'positive' | 'negative' | 'warning'
+
+type AlertProps = {
+  /**
+   * Set this to change alert kind
+   */
+  kind: Kind
+}
+
+const AlertStyled = styled('div')`
+  padding: 15px 20px;
+  background: white;
+  border-radius: 3px;
+  color: white;
+  background: ${({ kind = 'info' }) => kinds[kind]};
+`
+
+const Alert = ({ kind = 'info', ...props }: AlertProps) => (
+  <AlertStyled {...props} kind={kind} />
+)
+
+export default Alert
