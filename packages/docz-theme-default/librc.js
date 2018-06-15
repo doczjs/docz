f53f29d8449778e341f7bbe6b48d3cb1b8a63d00
Pedro Nauck <pedronauck@gmail.com>
2018-06-15 18:31:32 -0300

    chore(docz-theme-default): remove docz as peerdependency

--- packages/docz-theme-default/librc.js
+++ packages/docz-theme-default/librc.js
@@ -2,6 +2,7 @@ const svg = require('rollup-plugin-svg')
 
 module.exports = {
   plugins: [svg()],
+  external: ['docz', 'react', 'react-dom'],
   commonjs: {
     namedExports: {
       '../../node_modules/react-spinners/index.js': ['HashLoader'],
