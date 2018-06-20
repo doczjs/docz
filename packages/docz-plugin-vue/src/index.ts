20b42971ed9aae5698f054e7826734f9f0af3c26
Marcelo Formentão <marcelo@precisaosistemas.com.br>
2018-06-20 16:34:16 -0300

    fix(docz-plugin-vue): remove ts as type and include exclude in rules

--- packages/docz-plugin-vue/src/index.ts
+++ packages/docz-plugin-vue/src/index.ts
@@ -8,33 +8,32 @@ interface BabelRC {
   babelrc?: boolean
 }
 
-type ModifyBabelRC = (babelrc: BabelRC) => BabelRC
-type ModifyBundlerConfig<C = any> = (config: C, dev: boolean) => C
-
 export const doczPluginVue = () =>
   createPlugin({
-    modifyBabelRc: ((babelrc: BabelRC): BabelRC => {
+    modifyBabelRc: (babelrc: BabelRC): BabelRC => {
       if (babelrc.plugins === undefined) {
         babelrc.plugins = []
       }
       babelrc.plugins.push('vuera/babel')
       babelrc.babelrc = true
       return babelrc
-    }) as ModifyBabelRC,
+    },
 
-    modifyBundlerConfig: (config => {
+    modifyBundlerConfig: (config) => {
       config.module.rules.push({
         test: /\.vue$/,
         use: 'vue-loader',
+        exclude: /node_modules/
       })
 
       config.module.rules.push({
         test: /\.css$/,
         use: ['vue-style-loader', 'css-loader'],
+        exclude: /node_modules/
       })
 
       config.plugins.push(new VueLoaderPlugin())
 
       return config
-    }) as ModifyBundlerConfig,
+    },
   })
