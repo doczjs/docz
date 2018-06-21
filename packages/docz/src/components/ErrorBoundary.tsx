0a22ee3ab4401791ef4f933128edeb1c0851f01b
Pedro Nauck <pedronauck@gmail.com>
2018-06-21 17:13:35 -0300

    fix(docz): error boundary component

--- packages/docz/src/components/ErrorBoundary.tsx
+++ packages/docz/src/components/ErrorBoundary.tsx
@@ -1,49 +1,39 @@
 // Adapted from https://github.com/bvaughn/react-error-boundary/blob/master/src/ErrorBoundary.js
 
 import * as React from 'react'
+import { Component } from 'react'
 import { ErrorReporter as DefaultErrorReporter } from './ErrorReporter'
 
 interface ErrorInfo {
   componentStack: string
 }
 
-type ErrorBoundaryProps = {
+export interface ErrorBoundaryProps {
   ErrorReporter?: React.ComponentClass<any>
   onError?: (error: Error, componentStack: string) => void
-} & Partial<DefaultProps>
+}
 
 export interface ErrorBoundaryState {
   error: Error | null
   errorInfo: ErrorInfo | null
 }
 
-type DefaultProps = Readonly<typeof defaultProps>
-
-const defaultProps = {
-  onError: () => {},
-  ErrorReporter: DefaultErrorReporter,
-}
-
-export class ErrorBoundary extends React.Component<
+export class ErrorBoundary extends Component<
   ErrorBoundaryProps,
   ErrorBoundaryState
 > {
-  public static defaultProps = defaultProps
-
   public state = {
     error: null,
     errorInfo: null,
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    const { onError = () => null } = this.props
+
     this.setState({ error, errorInfo }, () => {
-      if (typeof this.props.onError === 'function') {
+      if (typeof onError === 'function') {
         try {
-          this.props.onError.call(
-            this,
-            error,
-            errorInfo ? errorInfo.componentStack : ''
-          )
+          onError.call(this, error, errorInfo ? errorInfo.componentStack : '')
         } catch (onErrorError) {
           console.warn('The supplied onError function threw and error:')
           console.warn(onErrorError)
@@ -52,12 +42,12 @@ export class ErrorBoundary extends React.Component<
     })
   }
 
-  public render() {
-    const { children, ErrorReporter } = this.props
+  public render(): React.ReactNode {
+    const { children, ErrorReporter = DefaultErrorReporter } = this.props
     const { error, errorInfo } = this.state
 
     if (error !== null) {
-      console.log(error)
+      console.error(error)
       return <ErrorReporter error={error} errorInfo={errorInfo} />
     }
 
