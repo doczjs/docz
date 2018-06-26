02fb75d67bc4886ed281caa460704ff6f8d61b62
Marcelo Formentão <marceloavf3@hotmail.com>
2018-06-25 22:48:38 -0300

    feat(docz-example-vue): include draft component

---
+++ examples/vue/src/components/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="alert">
+    <slot></slot>
+    {{ kind }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Alert',
+    props: {
+      kind: {
+        type: String,
+        default: 'info'
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .alert {
+    background-color: grey;
+  }
+</style>
