8399aadde290b990377ba97d3512fa88c1cd5f2f
Pedro Nauck <pedronauck@gmail.com>
2018-07-19 01:48:22 -0300

    fix(docz-theme-default): open menu if has some link active

--- packages/docz-theme-default/src/components/shared/Sidebar/Menu.tsx
+++ packages/docz-theme-default/src/components/shared/Sidebar/Menu.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react'
-import { SFC } from 'react'
+import { Component } from 'react'
 import { Entry } from 'docz'
-import { Toggle } from 'react-powerplug'
 import ChevronDown from 'react-feather/dist/icons/chevron-down'
 import styled from 'react-emotion'
 
-import { Link, linkStyle } from './Link'
+import { Link, linkStyle, getActiveFromClass } from './Link'
 
 const Wrapper = styled('div')`
   display: flex;
   flex-direction: column;
+`
+
+interface ListProps {
+  opened: boolean
+}
+
+const List = styled('dl')`
+  visibility: ${(p: ListProps) => (p.opened ? 'visible' : 'hidden')};
+  max-height: ${(p: ListProps) => (p.opened ? '9999px' : '0px')};
+  transition: max-height 0.1s;
 `
 
 export const MenuLink = styled('a')`
@@ -42,41 +51,69 @@ export interface MenuProps {
   collapseAll: boolean
 }
 
-export const Menu: SFC<MenuProps> = ({
-  menu,
-  docs,
-  sidebarToggle,
-  collapseAll,
-}) => (
-  <Toggle initial={false}>
-    {({ on, toggle }: any) => {
-      const show = collapseAll || on
-      const handleToggle = (ev: React.SyntheticEvent<any>) => {
-        ev.preventDefault()
-        toggle()
-      }
+export interface MenuState {
+  opened: boolean
+  hasActive: boolean
+}
 
-      return (
-        <Wrapper>
-          <MenuLink href="#" onClick={handleToggle}>
-            {menu}
-            <Icon opened={show}>
-              <ChevronDown size={15} />
-            </Icon>
-          </MenuLink>
-          {show && (
-            <dl>
-              {docs.map(doc => (
-                <dt key={doc.id}>
-                  <Link onClick={sidebarToggle} to={doc.route} doc={doc}>
-                    {doc.name}
-                  </Link>
-                </dt>
-              ))}
-            </dl>
-          )}
-        </Wrapper>
-      )
-    }}
-  </Toggle>
-)
+export class Menu extends Component<MenuProps, MenuState> {
+  public $els: HTMLElement[]
+  public state: MenuState = {
+    opened: false,
+    hasActive: false,
+  }
+
+  constructor(props: MenuProps) {
+    super(props)
+    this.$els = []
+  }
+
+  public componentDidMount(): void {
+    this.checkActiveLink()
+  }
+
+  public render(): JSX.Element {
+    const { menu, docs, sidebarToggle, collapseAll } = this.props
+    const show = collapseAll || this.state.opened
+    const handleToggle = (ev: React.SyntheticEvent<any>) => {
+      ev.preventDefault()
+      this.toggle()
+    }
+
+    return (
+      <Wrapper>
+        <MenuLink href="#" onClick={handleToggle}>
+          {menu}
+          <Icon opened={show}>
+            <ChevronDown size={15} />
+          </Icon>
+        </MenuLink>
+        <List opened={show}>
+          {docs.map(doc => (
+            <dt key={doc.id}>
+              <Link
+                onClick={sidebarToggle}
+                to={doc.route}
+                doc={doc}
+                ref={(node: any) => {
+                  this.$els = this.$els.concat([node])
+                }}
+              >
+                {doc.name}
+              </Link>
+            </dt>
+          ))}
+        </List>
+      </Wrapper>
+    )
+  }
+
+  private toggle = (): void => {
+    this.setState(state => ({ opened: !state.opened }))
+  }
+
+  private checkActiveLink = (): void => {
+    const hasActive = this.$els.some(({ $el }: any) => getActiveFromClass($el))
+    if (hasActive) this.setState({ hasActive, opened: true })
+  }
+}
