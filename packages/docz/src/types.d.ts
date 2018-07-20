c1f4c6fc0fa22baa2eea0250dae62f5e1691d862
Pedro Nauck <pedronauck@gmail.com>
2018-07-20 18:56:18 -0300

    fix(docz): humanize prop types on PropTable

--- packages/docz/src/types.d.ts
+++ packages/docz/src/types.d.ts
@@ -1,5 +1,6 @@
 declare module '@sindresorhus/slugify'
 declare module '@mdx-js/tag'
 declare module '@mdx-js/tag/dist/mdx-provider'
+declare module 'capitalize'
 declare module 'react-router-hash-link'
 declare module 'pascalcase'
