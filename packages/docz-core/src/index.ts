259b72d53dfb7cd7b724b8ad6aeebc9c08fde8f0
Pedro Nauck <pedronauck@gmail.com>
2018-07-30 04:33:26 -0300

    feat(docz-core): support env files to set configuration (#171)
    * feat(docz-core): expose filtered env variables (#140)
    
    * refactor(docz-core): support for env config

--- packages/docz-core/src/index.ts
+++ packages/docz-core/src/index.ts
@@ -2,6 +2,7 @@ import * as commands from './commands'
 import { args } from './commands/args'
 
 export { commands, args }
+export { setEnv } from './config/env'
 export { Config } from './commands/args'
-export { Plugin, createPlugin } from './Plugin'
 export { BabelRC } from './utils/babel-config'
+export { Plugin, createPlugin } from './Plugin'
--- packages/docz/bin/index.js
+++ packages/docz/bin/index.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
-const envDotProp = require('env-dot-prop')
-const { args: defaultArgs } = require('docz-core')
-
-const setEnv = val => {
-  envDotProp.set('babel.env', val)
-  envDotProp.set('node.env', val)
-}
-
-const execCommand = (cmd, args) => {
-  require('docz-core').commands[cmd](args)
-}
+const { args: defaultArgs, setEnv } = require('docz-core')
+const execCommand = cmd => args => require('docz-core').commands[cmd](args)
 
 yargs
-  .command('dev', 'initialize docz dev server', defaultArgs, args => {
-    setEnv('development')
-    execCommand('dev', args)
-  })
-  .command('build', 'build dir as static site', defaultArgs, args => {
-    setEnv('production')
-    execCommand('build', args)
-  })
+  .command(
+    'dev',
+    'initialize docz dev server',
+    defaultArgs('development'),
+    execCommand('dev')
+  )
+  .command(
+    'build',
+    'build dir as static site',
+    defaultArgs('production'),
+    execCommand('build')
+  )
   .demandCommand()
   .help()
   .wrap(72)
