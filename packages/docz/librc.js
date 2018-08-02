5625d961cb0f309333971a4e8f53f9ba2dadc6a4
Pedro Nauck <pedronauck@gmail.com>
2018-08-02 19:10:12 -0300

    chore(docz): dependencies and bundle adjustments

--- packages/docz/librc.js
+++ packages/docz/librc.js
@@ -1,7 +1,15 @@
 const pkg = require('./package.json')
 
+const internal = [
+  'create-react-context',
+  'react-copy-write',
+  'react-imported-component',
+]
+
+const external = Object.keys(pkg.dependencies)
+  .concat(['@mdx-js/tag/dist/mdx-provider'])
+  .filter(dep => internal.indexOf(dep) === -1)
+
 module.exports = {
-  external: Object.keys(pkg.dependencies).concat([
-    '@mdx-js/tag/dist/mdx-provider',
-  ]),
+  external,
 }
