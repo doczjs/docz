e8e8ec8531c7d2d45ae04985cf556aa727fde703
Pedro Nauck <pedronauck@gmail.com>
2018-08-02 18:21:19 -0300

    fix(docz-theme-default): table layout adjustment

--- packages/docz-theme-default/src/components/ui/Table.tsx
+++ packages/docz-theme-default/src/components/ui/Table.tsx
@@ -3,7 +3,7 @@ import styled from 'react-emotion'
 export const Table = styled('table')`
   width: 100%;
   padding: 0;
-  table-layout: fixed;
+  table-layout: auto;
   box-shadow: 0 0 0 1px ${p => p.theme.docz.colors.border};
   background-color: transparent;
   border-spacing: 0;
