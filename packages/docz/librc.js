624dce25321ad0abb4e58c97570c2e3da1072195
Pedro Nauck <pedronauck@gmail.com>
2018-08-02 19:13:57 -0300

    chore(docz): remove internal deps

--- packages/docz/librc.js
+++ packages/docz/librc.js
@@ -1,14 +1,8 @@
 const pkg = require('./package.json')
 
-const internal = [
-  'create-react-context',
-  'react-copy-write',
-  'react-imported-component',
-]
-
-const external = Object.keys(pkg.dependencies)
-  .concat(['@mdx-js/tag/dist/mdx-provider'])
-  .filter(dep => internal.indexOf(dep) === -1)
+const external = Object.keys(pkg.dependencies).concat([
+  '@mdx-js/tag/dist/mdx-provider',
+])
 
 module.exports = {
   external,
