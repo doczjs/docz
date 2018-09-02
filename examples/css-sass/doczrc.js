6d2dc2a9b65bf9641af88fdf16a60a75f295512e
Pedro Nauck <pedronauck@gmail.com>
2018-09-01 21:21:14 -0300

    chore: add titles in some examples

--- examples/css-sass/doczrc.js
+++ examples/css-sass/doczrc.js
@@ -2,6 +2,7 @@ import { css } from 'docz-plugin-css'
 
 export default {
   plugins: [
+    title: 'CSS Sass',
     css({
       preprocessor: 'sass',
       cssmodules: true,
--- examples/typescript/doczrc.js
+++ examples/typescript/doczrc.js
@@ -1,3 +1,4 @@
 module.exports = {
+  title: 'Docz Typescript',
   typescript: true,
 }
