ba5108694d5ebe71f5bb5ace1f7d96edbd8e3e41
Pedro Nauck <pedronauck@gmail.com>
2018-09-02 19:00:28 -0300

    fix(docz-core): dispose hmr on imports

--- packages/docz-core/templates/root.tpl.js
+++ packages/docz-core/templates/root.tpl.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 import Theme from '<%- theme %>'
+
+import { imports } from './imports'
 import db from './db.json'
 
 <% if (wrapper) {%>import Wrapper from '<%- wrapper %>'<%}%>
 
-const Root = ({ imports }) => (
+const Root = () => (
   <Theme
     db={db}
     imports={imports}
