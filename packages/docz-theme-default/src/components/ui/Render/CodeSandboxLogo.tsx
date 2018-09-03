7511e40fbd22fca1f6a4e7d590c9f33b6ae974eb
Ives van Hoorne <ives@fb.com>
2018-09-04 00:10:20 +0100

    feat(docz): add support for exporting to CodeSandbox
    This allows you to export the code examples directly to CodeSandbox from the docs

---
+++ packages/docz-theme-default/src/components/ui/Render/CodeSandboxLogo.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+
+export default ({
+  width,
+  height,
+  style,
+}: {
+  width?: number
+  height?: number
+  style?: any
+}) => (
+  <svg
+    style={style}
+    width={width}
+    height={height}
+    viewBox="0 0 512 512"
+    version="1.1"
+  >
+    <title>favicon</title>
+    <desc>Created with Sketch.</desc>
+    <defs />
+    <g
+      id="favicon"
+      stroke="none"
+      stroke-width="1"
+      fill="none"
+      fill-rule="evenodd"
+    >
+      <path
+        d="M69.2898098,165.083974 L69.2898098,276.649443 L152.161311,324.692718 L152.161311,412.603224 L241.327633,463.829131 L241.327633,264.06328 L69.2898098,165.083974 Z M89.0172642,137.098529 L260.121958,235.540974 L427.640018,138.456525 L339.210941,87.2017661 L262.258901,131.853758 L179.736828,84.2839889 L89.0172642,137.098529 Z M272.206216,463.739666 L370.845646,406.905256 L370.845646,322.809124 L444.244039,280.276172 L444.244039,167.397587 L272.206216,266.116045 L272.206216,463.739666 Z M255.633239,512 L34,384.729507 L34,128.977638 L255.644267,0 L477.328236,128.432852 L477.328236,384.321468 L255.633239,512 Z"
+        fill="currentColor"
+        fill-rule="nonzero"
+      />
+    </g>
+  </svg>
+)
--- packages/rehype-docz/src/index.ts
+++ packages/rehype-docz/src/index.ts
@@ -2,10 +2,12 @@ import is from 'unist-util-is'
 import nodeToString from 'hast-util-to-string'
 import strip from 'strip-indent'
 import flatten from 'lodash.flatten'
+import * as path from 'path'
 
 import { codeFromNode } from './code-from-node'
-import { importsFromEntry } from './imports-from-entry'
+import { importsFromEntry, scopesFromEntry } from './imports-from-entry'
 import { format } from './format'
+import { getCodeSandboxFiles } from './codesandbox-files'
 
 const componentName = (value: any) => {
   const match = value.match(/^\<\\?(\w+)/)
@@ -27,40 +29,57 @@ const removePlayground = (code: string) => {
   return code.replace(open(code), '').replace(close(code), '')
 }
 
-const addCodeProp = (imports: string[]) => async (node: any, idx: number) => {
+const sanitizeCode = (code: string) =>
+  strip(code)
+    .trim()
+    .replace(/'/g, `\\'`)
+    .replace(/`/g, '\\`')
+
+const addCodeProp = (
+  scopes: string[],
+  imports: string[],
+  cwd: string
+) => async (node: any, idx: number) => {
   const name = componentName(node.value)
   const tagOpen = new RegExp(`^\\<${name}`)
 
   if (isPlayground(name)) {
     const formatted = await format(nodeToString(node))
     const code = formatted.slice(1, Infinity)
-    const scope = `{props,${imports.join(',')}}`
-    const child = strip(removePlayground(code))
-      .trim()
-      .replace(/'/g, `\\'`)
-      .replace(/`/g, '\\`')
+    const scope = `{props,${scopes.join(',')}}`
+    const child = sanitizeCode(removePlayground(code))
+
+    let codeSandboxImportInfo: string | undefined
+    try {
+      const { parameters } = await getCodeSandboxFiles(child, imports, cwd)
+      codeSandboxImportInfo = parameters
+    } catch (e) {
+      console.error('Could not create Open in CodeSandbox', e)
+    }
 
     node.value = node.value.replace(
       tagOpen,
-      `<${name} __position={${idx}} __code={\`${child}\`} __scope={${scope}}`
+      `<${name} __position={${idx}} __codesandbox={\`${codeSandboxImportInfo}\`} __code={\`${child}\`} __scope={${scope}}`
     )
   }
 }
 
 const getScope = (node: any, idx: number) => {
-  return importsFromEntry(node)
+  return scopesFromEntry(node)
 }
 
-export default () => (tree: any) => {
-  const imports: string[] = flatten(
-    tree.children.filter((node: any) => is('import', node)).map(getScope)
-  )
+const getImports = (nodes: any[]) => {
+  return importsFromEntry(nodes)
+}
 
-  const nodes = [
-    tree.children
-      .filter((node: any) => is('jsx', node))
-      .map(addCodeProp(imports)),
-  ]
+export default () => (tree: any, fileInfo: any) => {
+  const importNodes = tree.children.filter((node: any) => is('import', node))
+  const imports: string[] = flatten(importNodes.map(getImports))
+  const scopes: string[] = flatten(importNodes.map(getScope))
+
+  const nodes = tree.children
+    .filter((node: any) => is('jsx', node))
+    .map(addCodeProp(scopes, imports, path.dirname(fileInfo.history[0])))
 
   return Promise.all(nodes).then(() => tree)
 }
