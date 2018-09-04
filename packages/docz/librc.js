a293aa44d54f3de83f6b56f74a06ae010051f003
Pedro Nauck <pedronauck@gmail.com>
2018-09-04 00:16:46 -0300

    fix(docz): add react router inside the docz bundle

--- packages/docz/librc.js
+++ packages/docz/librc.js
@@ -2,10 +2,13 @@ const pkg = require('./package.json')
 
 const internal = [
   '@sindresorhus/slugify',
-  'create-react-context',
   'array-sort',
   'capitalize',
+  'create-react-context',
   'react-imported-component',
+  'react-router',
+  'react-router-dom',
+  'react-router-hash-link',
 ]
 
 const depsExternal = ['@mdx-js/tag/dist/mdx-provider']
