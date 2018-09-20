66e6248ddf3d4af549dc2e7bcba167d9aa5c698c
Pedro Nauck <pedronauck@gmail.com>
2018-09-20 19:12:58 -0300

    fix(docz-core): data server rewriting files

--- packages/docz-core/src/states/config.ts
+++ packages/docz-core/src/states/config.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs-extra'
 import { load, finds } from 'load-cfg'
 import chokidar from 'chokidar'
-import equal from 'fast-deep-equal'
 import get from 'lodash.get'
 
 import { Params, State } from '../DataServer'
@@ -36,18 +35,13 @@ const getInitialConfig = (config: Config): Payload => {
   }
 }
 
-const updateConfig = (config: Config) => async (p: Params) => {
-  const old = p.state.config
-  const newConfig = load('docz', getInitialConfig(config), true, false)
-
-  if (newConfig && !equal(old, newConfig)) {
-    p.setState('config', newConfig)
-  }
-}
+const updateConfig = (config: Config) => async ({ setState }: Params) =>
+  setState('config', load('docz', getInitialConfig(config), true, false))
 
 export const state = (config: Config): State => {
   const watcher = chokidar.watch(finds('docz'), {
     cwd: paths.root,
+    ignored: /(^|[\/\\])\../,
     persistent: true,
   })
 
@@ -55,10 +49,11 @@ export const state = (config: Config): State => {
     init: updateConfig(config),
     update: async params => {
       const update = updateConfig(config)
+      const fn = async () => update(params)
 
-      watcher.on('add', async () => update(params))
-      watcher.on('change', async () => update(params))
-      watcher.on('unlink', async () => update(params))
+      watcher.on('add', fn)
+      watcher.on('change', fn)
+      watcher.on('unlink', fn)
 
       return () => watcher.close()
     },
