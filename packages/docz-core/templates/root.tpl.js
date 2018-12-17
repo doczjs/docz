2b78e969680b0ca71e8b1cd0f6450928ec54b68d
Pedro Nauck <pedronauck@gmail.com>
2018-12-17 21:39:57 -0200

    chore: remove setConfig for hot-loader

--- packages/docz-core/templates/root.tpl.js
+++ packages/docz-core/templates/root.tpl.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { setConfig, hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader'
 import Theme from '<%- theme %>'
 
 <% if (wrapper) {%>import Wrapper from '<%- wrapper %>'<%}%>
@@ -7,10 +7,5 @@ import Theme from '<%- theme %>'
 const Root = () => (
   <Theme <% if (wrapper) {%>wrapper={Wrapper}<%}%> />
 )
-
-setConfig({
-  ignoreSFC: true,
-  pureRender: true,
-})
 
 export default hot(module)(Root)
