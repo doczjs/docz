abb3626bce4f61b6ae132c0f39c501fbde646fd4
Pedro Nauck <pedronauck@gmail.com>
2018-12-19 14:55:28 -0200

    fix(docz-core): public folder in dev mode

--- packages/docz-core/src/webpack/devserver.ts
+++ packages/docz-core/src/webpack/devserver.ts
@@ -1,4 +1,5 @@
 import * as path from 'path'
+import * as express from 'express'
 import errorOverlayMiddleware from 'react-dev-utils/errorOverlayMiddleware'
 import evalSourceMapMiddleware from 'react-dev-utils/evalSourceMapMiddleware'
 import ignoredFiles from 'react-dev-utils/ignoredFiles'
@@ -9,13 +10,15 @@ import { ServerHooks } from '../Bundler'
 
 export const devServerConfig = (hooks: ServerHooks, args: Args) => {
   const srcPath = path.resolve(paths.root, args.src)
-  const publicDir = path.join(paths.root, args.public)
+  const publicDirPath = path.resolve(paths.root, args.public)
+  const publicDir = path.join(paths.root, publicDirPath)
+  const nonExistentDir = path.resolve(__dirname, 'non-existent')
 
   return {
     publicPath: '/',
     compress: true,
     clientLogLevel: args.debug ? 'info' : 'none',
-    contentBase: publicDir,
+    contentBase: [nonExistentDir],
     watchContentBase: true,
     hot: true,
     quiet: !args.debug,
@@ -32,6 +35,7 @@ export const devServerConfig = (hooks: ServerHooks, args: Args) => {
     before(app: any, server: any): void {
       app.use(evalSourceMapMiddleware(server))
       app.use(errorOverlayMiddleware())
+      app.use('/public', express.static(publicDir))
       hooks.onPreCreateApp<any>(app)
     },
     after(app: any): void {
