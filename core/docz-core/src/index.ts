a95d75f100217480f7a34064614b06770e21b162
Pedro Nauck <pedronauck@gmail.com>
2019-01-18 10:54:47 -0200

    chore(docz-core): export parseConfig method

--- core/docz-core/src/index.ts
+++ core/docz-core/src/index.ts
@@ -1,6 +1,12 @@
+/** cli exports */
 export { cli } from './cli'
+
+/** config exports */
+export { parseConfig } from './config/docz'
 export { Config } from './config/argv'
 export { BabelRC } from './config/babel'
+
+/** lib exports */
 export { Plugin, createPlugin } from './lib/Plugin'
 export { DataServer } from './lib/DataServer'
 export { Entries } from './lib/Entries'
