4162a1c169550a1987ec03ebe6246761f2b84374
Pedro Nauck <pedronauck@gmail.com>
2019-01-22 11:27:54 -0200

    chore(docz): remove callbags for state subcribe

--- core/docz/src/utils/createState.tsx
+++ core/docz/src/utils/createState.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import { createContext, Component } from 'react'
 import equal from 'fast-deep-equal'
-import observe from 'callbag-observe'
-import makeSubject from 'callbag-subject'
 
 export interface ProviderProps<T> {
   initial?: T
@@ -10,29 +8,35 @@ export interface ProviderProps<T> {
 
 export type PrevState<T> = (prevState: T) => T
 export type GetFn<T> = (state: T) => React.ReactNode
+export type Dispatch<T> = T | PrevState<T>
 
 export interface State<T> {
   get: (fn: GetFn<T>) => React.ReactNode
-  set: (param: T | PrevState<T>) => void
+  set: (param: Dispatch<T>) => void
   Provider: React.ComponentType<ProviderProps<T>>
 }
 
-const subject = makeSubject()
 export function create<T = any>(initial: T = {} as T): State<T> {
   const { Provider, Consumer }: any = createContext<T>(initial)
+  const listeners = new Set()
+
   Consumer.displayName = 'StateConsumer'
+
+  const dispatch = (fn: Dispatch<T>) => {
+    listeners.forEach(listener => listener(fn))
+  }
 
   return {
     get: fn => <Consumer>{fn}</Consumer>,
-    set: fn => subject(1, fn),
+    set: fn => dispatch(fn),
     Provider: class CustomProvider extends Component<ProviderProps<T>, T> {
       public static displayName = 'StateProvider'
       public state: T = this.props.initial || initial
       public componentDidMount(): void {
-        observe((fn: T) => this.setState(fn))(subject)
+        listeners.add((fn: Dispatch<T>) => this.setState(fn))
       }
       public componentWillUnmount(): void {
-        subject(2)
+        listeners.clear()
       }
       public shouldComponentUpdate(nextProps: any, nextState: any): boolean {
         return !equal(this.state, nextState)
