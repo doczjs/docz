58b3eb5c81484a0b95cdb85f93992ef28bf6d2db
Jess Chadwick <jesschadwick@gmail.com>
2019-04-01 12:55:17 -0400

    feat(docz-theme-default): add Editor to component exports (#750)

--- core/docz-theme-default/src/components/ui/index.ts
+++ core/docz-theme-default/src/components/ui/index.ts
@@ -1,5 +1,6 @@
 import { Playground } from './Playground'
 import { Blockquote } from './Blockquote'
+import { Editor } from './Editor'
 import { H1 } from './H1'
 import { H2 } from './H2'
 import { H3 } from './H3'
@@ -22,6 +23,7 @@ import { UnorderedList } from './UnorderedList'
 export const components = {
   a: Link,
   blockquote: Blockquote,
+  editor: Editor,
   h1: H1,
   h2: H2,
   h3: H3,
