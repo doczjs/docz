80b095656fb197402d312434e3ebc540294a3cf8
Jed Mao <jedmao@users.noreply.github.com>
2019-04-02 10:16:27 -0500

    fix(docz-core): default ignore paths (#754)

--- core/docz-core/src/config/docz.ts
+++ core/docz-core/src/config/docz.ts
@@ -30,11 +30,11 @@ export const doczRcBaseConfig = {
   mdPlugins: [],
   hastPlugins: [],
   ignore: [
-    'readme.md',
-    'changelog.md',
-    'code_of_conduct.md',
-    'contributing.md',
-    'license.md',
+    '**/readme.md',
+    '**/changelog.md',
+    '**/code_of_conduct.md',
+    '**/contributing.md',
+    '**/license.md',
   ],
 }
 
