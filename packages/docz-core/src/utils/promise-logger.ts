6e3896240e5a30466d8b9373a3b1a292068bcd7e
Ollie Relph <ollie@relph.me>
2019-04-19 09:55:34 +0100

    fix(docz-core): Reduce logging for CI

--- packages/docz-core/src/utils/promise-logger.ts
+++ packages/docz-core/src/utils/promise-logger.ts
@@ -1,13 +1,26 @@
 import * as path from 'path'
 import * as fs from 'fs-extra'
+import logger from 'signale'
 import createLogger from 'progress-estimator'
 import * as paths from '../config/paths'
 
 const CACHE_DIR = path.join(paths.cache, '.progress-estimator')
 
-export const promiseLogger = async (...args: any[]) => {
-  await fs.ensureDir(CACHE_DIR)
-  return createLogger({
-    storagePath: path.join(paths.cache, '.progress-estimator'),
-  })(...args)
+const isCi = () =>
+  !!(
+    process.env.CI || // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari
+    process.env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
+    process.env.BUILD_NUMBER || // Jenkins, TeamCity
+    process.env.RUN_ID
+  ) // TaskCluster, dsari
+
+export const promiseLogger = async (promise: any, message: string) => {
+  if (process.stdout.isTTY && !isCi()) {
+    await fs.ensureDir(CACHE_DIR)
+    return createLogger({
+      storagePath: path.join(paths.cache, '.progress-estimator'),
+    })(promise, message)
+  }
+  logger.log(`${message}`)
+  return promise
 }
