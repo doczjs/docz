4f1a87f15dd3be5f854732ab3452bfd04ae40e25
rakannimer <rakannimer@gmail.com>
2019-08-25 11:55:32 +0300

    fix(docz-example-gatsby): make gatsby example runnable outside repo

--- examples/gatsby/gatsby-config.js
+++ examples/gatsby/gatsby-config.js
@@ -1,29 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: `Docz POC`,
-    description: 'This is just an experiment to integrate Docz and Gatsby',
-    author: 'Pedro Nauck',
-  },
-  /**
-   * ## IMPORTANT ##
-   * Do you shouldn't need this plugins
-   * You need them just because of our monorepo
-   * */
   plugins: [
-    {
-      resolve: 'gatsby-theme-docz',
-    },
-    {
-      resolve: 'gatsby-plugin-eslint',
-      options: {
-        test: /\.js$|\.jsx$/,
-        exclude: /(node_modules|.cache|public|docz\/core\/docz)/,
-        stages: ['develop'],
-        options: {
-          emitWarning: true,
-          failOnError: false,
-        },
-      },
-    },
+    'gatsby-theme-docz',
+    `gatsby-transformer-sharp`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
