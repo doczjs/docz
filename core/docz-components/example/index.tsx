f7f14bbce0064903aa7a9ca58d2bda9a2f39ca25
rakannimer <rakannimer@gmail.com>
2019-08-31 12:15:24 +0300

    fix(docz-components): fix development example

--- core/docz-components/example/index.tsx
+++ core/docz-components/example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Playground } from '../.';
+import { Playground } from '../';
 
 const App = () => {
   return (
--- core/docz-components/src/index.tsx
+++ core/docz-components/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
 // Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export const Playground = () => {
+  return <div>the snozzb erries taste like snozzberries</div>;
 };
