d2aff82fb941ca4bcb1d2b01649655c84c94c5c8
rakannimer <rakannimer@gmail.com>
2019-09-02 14:39:09 +0300

    fix(docz-core): make onCreateWebpackConfig extendable

--- core/docz-core/templates/gatsby-node.tpl.js
+++ core/docz-core/templates/gatsby-node.tpl.js
@@ -1,11 +1,29 @@
 const path = require('path')
+const get = require('lodash/get')
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
+const NO_OP = () => {}
+
+let gatsbyNodeCustom
+try {
+  gatsbyNodeCustom = require('./gatsby-node.custom')
+} catch (err) {
+  gatsbyNodeCustom = {
+    onCreateWebpackConfig: NO_OP,
+  }
+}
+
+exports.onCreateWebpackConfig = args => {
+  args.actions.setWebpackConfig({
     resolve: {
       alias: {
         react: path.resolve('../node_modules/react'),
       },
     },
   })
+  const onCreateWebpackConfig = get(
+    gatsbyNodeCustom,
+    'onCreateWebpackConfig',
+    NO_OP
+  )
+  onCreateWebpackConfig(args)
 }
