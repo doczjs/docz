fe984fde012d12d5a9e8d745349908aa22f5a8aa
rakannimer <rakannimer@gmail.com>
2019-09-03 16:21:28 +0300

    fix(docz-core): run init on build if docz is not initialized

--- core/docz-core/src/commands/build.ts
+++ core/docz-core/src/commands/build.ts
@@ -3,10 +3,21 @@ import * as logger from 'signale'
 
 import { parseConfig } from '../config/docz'
 import { bundler as gatsby } from '../bundler'
+import { getIsFirstInstall } from '../bundler/machine/actions'
+import { init } from './init'
 
 export const build = async (args: Arguments<any>) => {
   const config = await parseConfig(args)
   const bundler = gatsby(config)
+  const isFirstInstall = getIsFirstInstall()
+
+  if (isFirstInstall) {
+    try {
+      await init(args)
+    } catch (err) {
+      throw new Error(`Failed to initialize docz : ${err.message}`)
+    }
+  }
 
   try {
     await bundler.build()
