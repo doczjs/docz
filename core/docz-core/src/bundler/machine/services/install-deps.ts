679017912b9144305bbe8f3f3155c67e80315db1
rakannimer <rakannimer@gmail.com>
2019-09-04 13:53:11 +0300

    feat(docz-core): remove installation step in .docz 🎉

--- core/docz-core/src/bundler/machine/services/install-deps.ts
+++ core/docz-core/src/bundler/machine/services/install-deps.ts
@@ -1,36 +1,4 @@
-import { pipe } from 'lodash/fp'
-import spawn from 'cross-spawn'
-import chalk from 'chalk'
-import sh from 'shelljs'
-import ora from 'ora'
-
-import * as paths from '../../../config/paths'
-import { ServerMachineCtx } from '../context'
-
-const warn: Function = pipe(
-  chalk.yellow,
-  console.log
-)
-
-export const installDeps = async ({ firstInstall }: ServerMachineCtx) => {
-  if (firstInstall) {
-    warn('\n----------------')
-    warn(`We need to install some dependencies in order to run your bundler.`)
-    warn('This just happens the first time you run docz.')
-    warn('This could take a while !')
-    warn('----------------\n')
-  }
-
-  return new Promise(async (resolve, reject) => {
-    sh.cd(paths.docz)
-
-    const checking = !firstInstall && ora('Checking dependencies...').start()
-    const result = spawn.sync('yarn', ['--production=false'], {
-      stdio: 'inherit',
-    })
-
-    if (checking) checking.succeed('Dependencies checked!')
-    if (result.error) return reject(result.error)
-    return resolve()
-  })
+export const installDeps = async () => {
+  // No need to install dependencies
+  return
 }
