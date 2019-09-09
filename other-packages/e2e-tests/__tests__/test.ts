8641675df2d4e42b5286e986c66799c6a73da794
rakannimer <rakannimer@gmail.com>
2019-09-09 12:39:05 +0300

    test(e2e-tests): more TestCafe tests

--- other-packages/e2e-tests/__tests__/test.ts
+++ other-packages/e2e-tests/__tests__/test.ts
@@ -1,16 +1,30 @@
 import { Selector } from 'testcafe'
-// import { getByText } from '@testing-library/testcafe'
 
-const b: number = 2
+const getByTestId = async (testId: string) => {
+  return await Selector(`[data-testid=${testId}]`)
+}
+
 fixture`Renders a valid app`.page`http://localhost:3000/`
 
 test('Renders the right layout and nav', async t => {
-  // Hacky selectors pending theme update with test ids or labels
-  const main = await Selector('main')
-  await t.expect(main.childNodeCount).eql(2)
-  const header = await main.child(0)
-  const layout = await main.child(1)
+  const layout = await getByTestId('layout')
+  await t.expect(layout.exists).eql(true)
+  const header = await getByTestId('header')
+  await t.expect(header.exists).eql(true)
+  const sidebar = await getByTestId('sidebar')
+  await t.expect(sidebar.exists).eql(true)
+  const logo = await getByTestId('logo')
+  await t.expect(logo.exists).eql(true)
+  const navGroup = await getByTestId('nav-group')
+  await t.expect(navGroup.exists).eql(true)
+  await t.navigateTo('/src-components-alert')
 
-  // await t.expect(header.find('a').count).eql(1)
-  // await t.expect(layout.find('a').count).gte(3)
+  const playground = await getByTestId('playground')
+  await t.expect(playground.count).eql(2)
+
+  const livePreview = await getByTestId('live-preview')
+  await t.expect(livePreview.count).eql(2)
+
+  const liveEditor = await getByTestId('live-editor')
+  await t.expect(liveEditor.count).eql(2)
 })
