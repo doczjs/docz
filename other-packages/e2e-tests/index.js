d7f7ae2856dedeabdc8df9d49583dfa884395390
rakannimer <rakannimer@gmail.com>
2019-09-09 17:06:52 +0300

    ci: build and serve

--- other-packages/e2e-tests/index.js
+++ other-packages/e2e-tests/index.js
@@ -87,13 +87,14 @@ const ci = async () => {
     console.log(`Modifying package.json in ${example.tmp}`)
     const pathToPackageJson = path.join(`${example.tmp}`, 'package.json')
     const packageJson = await fs.readJson(pathToPackageJson)
-    // set(packageJson, `dependencies.gatsby-theme-docz`, paths.doczGatsbyTheme)
+    // set(packageJson, `dependencies.gabuitsby-theme-docz`, paths.doczGatsbyTheme)
     await fs.writeJson(pathToPackageJson, packageJson, { spaces: 2 })
 
     console.log(`Installing modules in tmp directory`)
     await installNodeModules(example.tmp, exampleName)
 
-    runCommand(`yarn dev --port 3000`, example.tmp)
+    await runCommand(`yarn build`, example.tmp)
+    runCommand(`yarn serve --port 3000`, example.tmp)
     await waitOn({ resources: ['http://localhost:3000'] })
     console.log('Ready. Starting e2e tests')
 
