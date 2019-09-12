50fdc8e0e7cdeff50d4682b9698a5ce261cc45b5
dangoo <daniel.gooss@sinnerschrader.com>
2019-09-12 20:55:04 +0200

    fix: prevents config.base to affect outdir  path

--- core/docz-core/src/commands/serve.ts
+++ core/docz-core/src/commands/serve.ts
@@ -3,8 +3,18 @@ import spawn from 'cross-spawn'
 import sh from 'shelljs'
 
 import * as paths from '../config/paths'
+import { parseConfig } from '../config/docz'
 
-export const serve = async (_args: Arguments<any>) => {
+export const serve = async (args: Arguments<any>) => {
+  const config = await parseConfig(args)
+  const cliArgs = ['serve']
+
+  if (typeof config.base === 'string' && config.base.length) {
+    // Append gatsby option `prefixPaths`to CLI args
+    // https://www.gatsbyjs.org/docs/path-prefix/
+    cliArgs.push('--prefixPaths')
+  }
+
   sh.cd(paths.docz)
-  spawn.sync('yarn', ['serve'], { stdio: 'inherit' })
+  spawn.sync('yarn', cliArgs, { stdio: 'inherit' })
 }
