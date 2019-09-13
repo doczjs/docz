044a97e611c639039fde4e9367ef689057d7728e
Daniel Gooß <daniel.gooss@sinnerschrader.com>
2019-09-13 06:50:58 +0200

    feat(docz-core): use config base for gatsby pathPrefix (#1103)
    * feat: path config to bundler & add pathPrefix
    
    * feat: add pathPrefix to gatsby config tpl.
    
    * fix: added quotes to option
    
    * fix: prevents config.base to affect outdir  path

--- core/docz-core/src/bundler/build.ts
+++ core/docz-core/src/bundler/build.ts
@@ -4,11 +4,19 @@ import spawn from 'cross-spawn'
 import sh from 'shelljs'
 
 import * as paths from '../config/paths'
+import { BuildFn } from '../lib/Bundler'
 
-export const build = async (dist: string) => {
+export const build: BuildFn = async (config, dist) => {
   const publicDir = path.join(paths.docz, 'public')
+  const cliArgs = ['build']
+
+  if (typeof config.base === 'string' && config.base.length) {
+    // Append gatsby option `prefixPaths`to CLI args
+    // https://www.gatsbyjs.org/docs/path-prefix/
+    cliArgs.push('--prefixPaths')
+  }
 
   sh.cd(paths.docz)
-  spawn.sync('yarn', ['build'], { stdio: 'inherit' })
+  spawn.sync('yarn', cliArgs, { stdio: 'inherit' })
   await fs.copy(publicDir, dist)
 }
