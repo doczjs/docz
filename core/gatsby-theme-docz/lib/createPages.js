8f89a7d6fb1fccef01907f14115f0f3736d4166e
rakannimer <rakannimer@gmail.com>
2019-09-28 22:27:40 -0400

    feat(gatsby-theme-docz): set first entry as root when no / route given

--- core/gatsby-theme-docz/lib/createPages.js
+++ core/gatsby-theme-docz/lib/createPages.js
@@ -1,4 +1,5 @@
 const { get } = require('lodash/fp')
+const { parseConfig } = require('docz-core')
 
 const ENTRIES_QUERY = `
   {
@@ -23,12 +24,27 @@ const ENTRIES_QUERY = `
   }
 `
 
-module.exports = ({ graphql, actions }) => {
-  return graphql(ENTRIES_QUERY).then(({ data, errors }) => {
+module.exports = async ({ graphql, actions }, opts = {}) => {
+  return graphql(ENTRIES_QUERY).then(async ({ data, errors }) => {
     const hasErrors = errors && errors.length > 0
     const entries = get('allDoczEntries.edges', data)
     if (!entries || entries.length === 0 || hasErrors) return
-
+    const defaultEntry = entries.find(({ node: entry }) => entry.route === '/')
+    if (defaultEntry === undefined) {
+      const config = await parseConfig(opts)
+      // Create a default entry unless specifically denied by config
+      const shouldNotCreateRootRoute = Boolean(config.noRootRoute)
+      if (shouldNotCreateRootRoute === false) {
+        // Set the first found entry as the default entry
+        const createdDefaultEntry = {
+          node: {
+            ...entries[0].node,
+            route: '/',
+          },
+        }
+        entries.unshift(createdDefaultEntry)
+      }
+    }
     entries.forEach(({ node: entry }) => {
       if (!entry) return
       actions.createPage({
