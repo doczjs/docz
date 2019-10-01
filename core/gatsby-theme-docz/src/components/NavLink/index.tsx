64b46d92e181cd9b6b6b968b8178d72faaa3355e
dangoo <daniel.gooss@sinnerschrader.com>
2019-10-01 23:59:41 +0200

    feat(gatsby-theme-docz): switch NavLink to ts

--- core/gatsby-theme-docz/src/components/NavLink/index.tsx
+++ core/gatsby-theme-docz/src/components/NavLink/index.tsx
@@ -2,35 +2,48 @@
 import React from 'react'
 import { jsx } from 'theme-ui'
 import { Link } from 'gatsby'
-import { useDocs, useCurrentDoc } from 'docz'
+import { useDocs, useCurrentDoc, MenuItem, Entry } from 'docz'
 import { get } from 'lodash/fp'
 
 import * as styles from './styles'
 
-const getHeadings = (route, docs) => {
+interface NavLinkProps extends Link<unknown> {
+  item: MenuItem
+}
+
+function getHeadings(docs: Entry[], route?: string) {
   const doc = docs.find(doc => doc.route === route)
   const headings = get('headings', doc)
   return headings ? headings.filter(heading => heading.depth === 2) : []
 }
 
-const getCurrentHash = () => {
+function getCurrentHash() {
   if (typeof window === 'undefined') {
     return ''
   }
   return window.location ? window.location.hash : ''
 }
 
-export const NavLink = ({ item, ...props }) => {
+export const NavLink: React.FunctionComponent<NavLinkProps> = ({
+  item,
+  ...props
+}) => {
   const docs = useDocs()
   const to = item.route
-  const headings = docs && getHeadings(to, docs)
+  const headings = docs ? getHeadings(docs, to) : []
   const current = useCurrentDoc()
   const isCurrent = item.route === current.route
   const showHeadings = isCurrent && headings && headings.length > 0
   const currentHash = getCurrentHash()
+
   return (
     <React.Fragment>
-      <Link {...props} to={to} sx={styles.link} activeClassName="active" />
+      <Link
+        {...props}
+        to={to || ''}
+        sx={styles.link}
+        activeClassName="active"
+      />
       {showHeadings &&
         headings.map(heading => (
           <Link
