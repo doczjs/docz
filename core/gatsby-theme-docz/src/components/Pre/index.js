f06c5cb775e1db3e49ded6aeaf96fe6cd011c5d5
dangoo <daniel.gooss@sinnerschrader.com>
2019-10-01 23:07:24 +0200

    feat(gatsby-theme-docz): switch Pre to ts

--- core/gatsby-theme-docz/src/components/Pre/index.js
+++
@@ -1,4 +0,0 @@
-/** @jsx jsx */
-import { jsx } from 'theme-ui'
-
-export const Pre = ({ children }) => <div>{children}</div>
---
+++ core/gatsby-theme-docz/src/components/Pre/index.tsx
@@ -0,0 +1,7 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import React from 'react'
+
+export const Pre: React.FunctionComponent = ({ children }) => (
+  <div>{children}</div>
+)
