047f1870672d7b24dfe77d1e93e24c764b9b2b84
dangoo <daniel.gooss@sinnerschrader.com>
2019-10-01 23:08:52 +0200

    feat(gatsby-theme-docz): switch NavSearch to ts

--- core/gatsby-theme-docz/src/components/NavSearch/index.tsx
+++ core/gatsby-theme-docz/src/components/NavSearch/index.tsx
@@ -3,8 +3,11 @@ import { jsx } from 'theme-ui'
 
 import * as styles from './styles'
 import { Search } from '../Icons'
+import React from 'react'
 
-export const NavSearch = props => {
+export const NavSearch = (
+  props: React.InputHTMLAttributes<HTMLInputElement>
+) => {
   return (
     <div sx={styles.wrapper} data-testid="nav-search">
       <Search size={20} sx={styles.icon} />
