46752b99dd052a99771ef5a1cc65c34b016cce0f
rakannimer <rakannimer@gmail.com>
2019-10-04 21:44:49 -0400

    ci(e2e-tests): re-add checks

--- other-packages/e2e-tests/index.js
+++ other-packages/e2e-tests/index.js
@@ -160,9 +160,15 @@ const runTests = async () => {
     console.log(`Modifying package.json in ${example.tmp}`)
 
     await updatePackageJson(example.tmp, pack => {
-      set(pack, 'dependencies.gatsby-theme-docz', 'ci')
-      set(pack, 'dependencies.docz', 'ci')
-      set(pack, 'dependencies.docz-core', 'ci')
+      if (get(pack, 'dependencies.gatsby-theme-docz', false)) {
+        set(pack, 'dependencies.gatsby-theme-docz', 'ci')
+      }
+      if (get(pack, 'dependencies.docz', false)) {
+        set(pack, 'dependencies.docz', 'ci')
+      }
+      if (get(pack, 'dependencies.docz-core', false)) {
+        set(pack, 'dependencies.docz-core', 'ci')
+      }
       return pack
     })
     console.log({ example })
