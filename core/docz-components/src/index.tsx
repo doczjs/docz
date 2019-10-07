4798a8d3d3bf8559356a7f218d1b7a53a139127b
dangoo <daniel.gooss@sinnerschrader.com>
2019-10-08 00:36:45 +0200

    feat(docz-components): expose theme contents

--- core/docz-components/src/index.tsx
+++ core/docz-components/src/index.tsx
@@ -1,1 +1,4 @@
 export { Playground, PlaygroundProps } from './components/Playground';
+export { default as getTheme } from './theme';
+export { media } from './theme/breakpoints';
+export { default as global } from './theme/global';
