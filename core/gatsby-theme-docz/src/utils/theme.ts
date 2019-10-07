2793530955b1796a99c0d2306928a268ab5c183c
dangoo <daniel.gooss@sinnerschrader.com>
2019-10-08 00:38:17 +0200

    feat(gatsby-theme-docz): translate utils to TS

---
+++ core/gatsby-theme-docz/src/utils/theme.ts
@@ -0,0 +1,24 @@
+import { useThemeUI } from 'theme-ui'
+import { get, defaultTo } from 'lodash'
+import { getTheme } from 'docz-components'
+import { PrismTheme } from 'prism-react-renderer'
+
+export const themeProp = (str: string) => (props: any) => {
+  return get(`theme.${str}`, props)
+}
+
+export const usePrismTheme = () => {
+  const themeUI = useThemeUI()
+  const prismTheme = get(themeUI.theme, 'prismTheme')
+  const colorMode: string = get(themeUI, 'colorMode', 'light')
+  const themeToUse = defaultTo<PrismTheme>(
+    prismTheme,
+    get(
+      themeUI.theme,
+      `prism.${colorMode}`,
+      get(getTheme(), `prism.${colorMode}`)
+    )
+  )
+
+  return themeToUse
+}
