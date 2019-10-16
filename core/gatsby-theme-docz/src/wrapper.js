4b700c672354f62ea2ee2456fb43a1b7a7b03fcc
Pedro Piñera <pedro@ppinera.es>
2019-10-16 11:35:37 +0200

    Don't destructure the attribute

--- core/gatsby-theme-docz/src/wrapper.js
+++ core/gatsby-theme-docz/src/wrapper.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-const Wrapper = ({ children, doc }) => (
+const Wrapper = ({ children }) => (
   <React.Fragment>{children}</React.Fragment>
 )
 export default Wrapper
