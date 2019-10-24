331bbaeaff9422527f3d42fea67903a2205e393c
rakannimer <rakannimer@gmail.com>
2019-10-24 16:37:11 +0300

    fix(docz): point to right path to hooks

--- core/docz/src/components/Playground.tsx
+++ core/docz/src/components/Playground.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { ComponentType, SFC } from 'react'
-import { useComponents } from 'src/hooks'
+import { useComponents } from '../hooks'
 
 export interface PlaygroundProps {
   className?: string
