e2eb2da141a821bd5d6f346ddc071e7372782ce6
rakannimer <rakannimer@gmail.com>
2019-10-24 16:26:21 +0300

    chore: delete unused docz-components

--- core/docz-components/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { PrismTheme } from 'prism-react-renderer';
-import { Theme as ThemeUI } from 'theme-ui';
-
-export interface Theme extends ThemeUI {
-  prism?: {
-    [key: string]: PrismTheme;
-  };
-}
