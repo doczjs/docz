32a3b3c1432247a4c255faf69f0b77812d2fd12a
Rakan Nimer <rakannimer@gmail.com>
2019-10-29 13:05:00 +0200

    Revert "feat(docz): expose <Code /> component" (#1210)
    This reverts commit c7a19b4fac2fa9c010ae440ba79454cf447f762b.

--- core/docz/src/index.ts
+++ core/docz/src/index.ts
@@ -5,6 +5,3 @@ export { Props, PropsComponentProps } from './components/Props'
 export * from './hooks'
 export { theme } from './theme'
 export { doczState, Entry, MenuItem, ThemeConfig } from './state'
-
-const { Code } = require('gatsby-theme-docz/src/components/Code')
-export { Code }
