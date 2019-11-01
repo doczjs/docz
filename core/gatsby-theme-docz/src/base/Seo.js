c1471bc488dface5cb40198c460dd6438db7533d
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2019-11-01 23:17:24 +0100

    chore(gatsby-theme-docz): switch out react-helmet
    This change switches out react-helmet with react-helmet-async. This
    change removes an annoying warning about deprecated lifecycle usage

--- core/gatsby-theme-docz/src/base/Seo.js
+++ core/gatsby-theme-docz/src/base/Seo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet-async'
 
 import { useDbQuery } from '../hooks/useDbQuery'
 
