06c89e70f579c1c0d4a20ecd64dd9f851e84e8cb
rakannimer <rakannimer@gmail.com>
2019-11-17 09:26:16 +0200

    docs: add logo in sidebar example

---
+++ examples/logo-in-sidebar/src/components/Alert.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import t from "prop-types";
+
+const kinds = {
+  info: "#5352ED",
+  positive: "#2ED573",
+  negative: "#FF4757",
+  warning: "#FFA502"
+};
+
+export const Alert = ({ children, kind, ...rest }) => (
+  <div
+    style={{
+      padding: 20,
+      borderRadius: 3,
+      color: "white",
+      background: kinds[kind] || "white"
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+Alert.propTypes = {
+  /**
+   * The kind prop is used to set the alert's background color
+   */
+  kind: t.oneOf(["info", "positive", "negative", "warning"])
+};
+
+Alert.defaultProps = {
+  kind: "info"
+};
---
+++ examples/logo-in-sidebar/src/gatsby-theme-docz/components/Sidebar/index.js
@@ -0,0 +1,58 @@
+/** @jsx jsx */
+import React, { useState, useRef, useEffect } from "react";
+import { Global } from "@emotion/core";
+import { jsx, Box } from "theme-ui";
+import { useMenus, useCurrentDoc } from "docz";
+
+import * as styles from "gatsby-theme-docz/src/components/Sidebar/styles";
+import { NavSearch } from "gatsby-theme-docz/src/components/NavSearch";
+import { NavLink } from "gatsby-theme-docz/src/components/NavLink";
+import { NavGroup } from "gatsby-theme-docz/src/components/NavGroup";
+
+export const Sidebar = React.forwardRef((props, ref) => {
+  const [query, setQuery] = useState("");
+  const menus = useMenus({ query });
+  const currentDoc = useCurrentDoc();
+  const currentDocRef = useRef();
+  const handleChange = ev => {
+    setQuery(ev.target.value);
+  };
+  useEffect(() => {
+    if (ref.current && currentDocRef.current) {
+      ref.current.scrollTo(0, currentDocRef.current.offsetTop);
+    }
+  }, []);
+  return (
+    <>
+      <Box onClick={props.onClick} sx={styles.overlay(props)}>
+        {props.open && <Global styles={styles.global} />}
+      </Box>
+      <Box ref={ref} sx={styles.wrapper(props)} data-testid="sidebar">
+        <img src="https://placekitten.com/200/100" />
+
+        <NavSearch
+          placeholder="Type to search..."
+          value={query}
+          onChange={handleChange}
+        />
+        {menus &&
+          menus.map(menu => {
+            if (!menu.route)
+              return <NavGroup key={menu.id} item={menu} sidebarRef={ref} />;
+            if (menu.route === currentDoc.route) {
+              return (
+                <NavLink key={menu.id} item={menu} ref={currentDocRef}>
+                  {menu.name}
+                </NavLink>
+              );
+            }
+            return (
+              <NavLink key={menu.id} item={menu}>
+                {menu.name}
+              </NavLink>
+            );
+          })}
+      </Box>
+    </>
+  );
+});
