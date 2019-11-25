c0e7109f6735df206b932ea5256dd8a8ece30085
rakannimer <rakannimer@gmail.com>
2019-11-25 10:53:02 +0200

    Revert "fix(gatsby-theme-docz): fix faulty webpack resolution"
    This reverts commit 3c2d6eb23ef6ae1d4e19db6355fe856ce1e50490.

--- core/gatsby-theme-docz/lib/onCreateWebpackConfig.js
+++ core/gatsby-theme-docz/lib/onCreateWebpackConfig.js
@@ -1,7 +1,13 @@
+const fs = require('fs-extra')
+const path = require('path')
 const { Plugin, parseConfig } = require('docz-core')
+
+const nodeModules = path.resolve(__dirname, 'node_modules')
+const parentNodeModules = path.resolve(__dirname, '../../../node_modules')
 
 module.exports = async (params, opts) => {
   const { stage, actions, getConfig } = params
+  const hasParentNodeModules = fs.pathExistsSync(parentNodeModules)
   const args = await parseConfig(opts)
   const run = Plugin.runPluginsMethod(args.plugins)
   const config = getConfig()
@@ -10,6 +16,14 @@ module.exports = async (params, opts) => {
     actions.setWebpackConfig({
       resolve: {
         extensions: config.resolve.extensions.concat(['.ts', '.tsx']),
+      },
+    })
+  }
+
+  if (hasParentNodeModules) {
+    actions.setWebpackConfig({
+      resolve: {
+        modules: [nodeModules, parentNodeModules],
       },
     })
   }
