37f409327424f90ce30c97e7841bc5b1d2add23d
rakannimer <rakannimer@gmail.com>
2019-11-29 15:57:06 +0200

    docs: initial

---
+++ examples/with-algolia-search/gatsby-config.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+throw process.env.GATSBY_ALGOLIA_APP_ID
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },
+  ],
+}
