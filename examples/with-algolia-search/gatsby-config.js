e76d45a066beb7980396e51d592d57c8b946e4ea
rakannimer <rakannimer@gmail.com>
2019-11-29 17:03:07 +0200

    docs: add post indexing

--- examples/with-algolia-search/gatsby-config.js
+++ examples/with-algolia-search/gatsby-config.js
@@ -1,5 +1,35 @@
 require('dotenv').config()
 
+const query = `{
+  pages: allMdx {
+    edges {
+      node {
+        objectID: id
+        frontmatter {
+          title
+          route
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest } }) => ({
+    ...frontmatter,
+    ...rest,
+  }))
+
+const settings = { attributesToSnippet: [`excerpt:20`] }
+const queries = [
+  {
+    query: query,
+    transformer: ({ data }) => flatten(data.pages.edges),
+    settings,
+  },
+]
+
 module.exports = {
   plugins: [
     {
@@ -7,6 +37,7 @@ module.exports = {
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
         queries,
         chunkSize: 10000, // default: 1000
       },
