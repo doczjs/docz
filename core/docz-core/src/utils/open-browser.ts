be0d6ffbffab8ec9b32ff4970de39239329ed410
Jesper Orb <jesperorb@gmail.com>
2020-05-05 09:50:39 +0200

    chore(docz-core): add deps explicitly (#1481)

--- core/docz-core/src/utils/open-browser.ts
+++ core/docz-core/src/utils/open-browser.ts
@@ -10,9 +10,9 @@
 var chalk = require('chalk')
 var execSync = require('child_process').execSync
 var spawn = require('cross-spawn')
-var opn = require('opn')
+var open = require('open')
 
-// https://github.com/sindresorhus/opn#app
+// https://github.com/sindresorhus/open#app
 var OSX_CHROME = 'google chrome'
 
 const Actions = Object.freeze({
@@ -24,7 +24,7 @@ const Actions = Object.freeze({
 function getBrowserEnv() {
   // Attempt to honor this environment variable.
   // It is specific to the operating system.
-  // See https://github.com/sindresorhus/opn#app for documentation.
+  // See https://github.com/sindresorhus/open#app for documentation.
   const value = process.env.BROWSER
   let action
   if (!value) {
@@ -86,7 +86,7 @@ function startBrowserProcess(browser: any, url: any) {
   }
 
   // Another special case: on OS X, check if BROWSER has been set to "open".
-  // In this case, instead of passing `open` to `opn` (which won't work),
+  // In this case, instead of passing `open` to `open` (which won't work),
   // just ignore it (thus ensuring the intended behavior, i.e. opening the system browser):
   // https://github.com/facebook/create-react-app/pull/1690#issuecomment-283518768
   if (process.platform === 'darwin' && browser === 'open') {
@@ -97,7 +97,7 @@ function startBrowserProcess(browser: any, url: any) {
   // (It will always open new tab)
   try {
     var options = { app: browser, wait: false }
-    opn(url, options).catch(() => {}) // Prevent `unhandledRejection` error.
+    open(url, options).catch(() => {}) // Prevent `unhandledRejection` error.
     return true
   } catch (err) {
     return false
