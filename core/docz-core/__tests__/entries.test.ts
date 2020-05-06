5e993ca9dacdfe4489b690d0faa5fba95519543a
jesperorb <jesperorb@gmail.com>
2020-05-06 11:33:25 +0200

    test(docz-core): match files and src pattern for glob

--- core/docz-core/__tests__/entries.test.ts
+++ core/docz-core/__tests__/entries.test.ts
@@ -1,4 +1,4 @@
-import { Entries } from '../src/lib/Entries'
+import { Entries, getFilesToMatch, matchFilesWithSrc } from '../src/lib/Entries'
 import { getTestConfig } from '../src/test-utils'
 
 describe('Entries', () => {
@@ -6,5 +6,36 @@ describe('Entries', () => {
     const config = getTestConfig()
     const entries = new Entries(config)
     expect(entries).toBeTruthy()
+  })
+
+  test('get map of entries', async () => {
+    const config = getTestConfig({
+      src: '__fixtures__',
+      files: '**/*.{mdx}',
+    })
+    const entries = new Entries(config)
+    const map = await entries.get()
+    expect(
+      Object.keys(map).includes('__fixtures__/Alert/Alert.mdx')
+    ).toBeTruthy()
+  })
+
+  test('with blank src and files', async () => {
+    const config = getTestConfig({
+      src: '',
+      files: 'docz/**/*.{mdx}',
+    })
+    const files = getFilesToMatch(config)
+    expect(matchFilesWithSrc(config)(files)[0]).toEqual('docz/**/*.{mdx}')
+  })
+  test('with src and files', async () => {
+    const config = getTestConfig({
+      src: '__fixtures__',
+      files: '**/*.{mdx}',
+    })
+    const files = getFilesToMatch(config)
+    expect(matchFilesWithSrc(config)(files)[0]).toEqual(
+      '__fixtures__/**/*.{mdx}'
+    )
   })
 })
