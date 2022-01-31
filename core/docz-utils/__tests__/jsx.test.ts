dca4450df416c4bbf5d7f6e0d60abe00fde264d1
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-01-31 20:02:33 +0100

    test(docz-utils): add test for current `removeTags` behavior

---
+++ core/docz-utils/__tests__/jsx.test.ts
@@ -0,0 +1,39 @@
+import { removeTags } from '../src/jsx'
+
+describe('removeTags', () => {
+  test('removes outer JSX tag', () => {
+    expect(
+      removeTags(`
+      <Playground>
+        <div>Some text</div>
+        <p>Other text</p>
+      </Playground>
+    `)
+    ).toMatchInlineSnapshot(`
+      "
+            
+              <div>Some text</div>
+              <p>Other text</p>
+            
+          "
+    `)
+  })
+
+  test('works when the closing tag is repeated in a comment', () => {
+    expect(
+      removeTags(`
+      <Playground>
+        {/* </Playground> */}
+        <div>Some text</div>
+      </Playground>
+    `)
+    ).toMatchInlineSnapshot(`
+      "
+            
+              {/*  */}
+              <div>Some text</div>
+            </Playground>
+          "
+    `)
+  })
+})
