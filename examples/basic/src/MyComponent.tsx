d7634ed4fb155c2da6e5933d937d62bfd9981255
Pedro Nauck <pedronauck@gmail.com>
2022-09-19 00:18:38 -0700

    chore(basic-example): improve example files

--- examples/basic/src/MyComponent.tsx
+++
@@ -1,3 +0,0 @@
-export function MyComponent() {
-  return <div>Hello world</div>;
-}
---
+++ examples/basic/src/components/Alert.tsx
@@ -0,0 +1,31 @@
+import type { ReactNode } from "react";
+
+const KINDS = {
+  info: "#5352ED",
+  positive: "#2ED573",
+  negative: "#FF4757",
+  warning: "#FFA502",
+};
+
+export type AlertProps = {
+  kind: "info" | "positive" | "negative" | "warning";
+  test: string;
+  children: ReactNode;
+};
+
+export function Alert({ children, kind = "info", ...rest }: AlertProps) {
+  return (
+    <div
+      style={{
+        padding: 20,
+        background: "white",
+        borderRadius: 3,
+        color: "white",
+        backgroundColor: KINDS[kind],
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
