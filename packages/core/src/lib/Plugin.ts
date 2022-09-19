0f0ecb6f29ba4a483f459acab687b43a61a59a41
Pedro Nauck <pedronauck@gmail.com>
2022-09-18 23:23:19 -0700

    refac(core): remove plugin methods

--- packages/core/src/lib/Plugin.ts
+++ packages/core/src/lib/Plugin.ts
@@ -9,32 +9,16 @@ type Entry = {
 };
 
 export type SetConfig = (config: Config) => Config | Promise<Config>;
-export type OnCreateBabelConfig = (params: any, dev: boolean) => void;
-export type OnCreateWebpackConfig<C = any> = (
-  config: C,
-  dev: boolean,
-  args: Config
-) => C;
 export type ModifyFiles = (files: string[], args: Config) => string[];
 export type ModifyEntry = (entry: Entry, args: Config) => Entry;
-export type OnCreateDevServer = <A>(app: A) => void;
-export type OnPreBuild = (args: Config) => void;
-export type OnPostBuild = (args: Config) => void;
-export type OnPreRender = () => void;
-export type OnPostRender = () => void;
 
 export interface PluginFactory {
   setConfig?: SetConfig;
-  onCreateBabelConfig?: OnCreateBabelConfig;
-  onCreateDevServer?: OnCreateDevServer;
-  onCreateWebpackConfig?: OnCreateWebpackConfig;
   modifyFiles?: ModifyFiles;
   modifyEntry?: ModifyEntry;
-  onPreBuild?: OnPreBuild;
-  onPostBuild?: OnPostBuild;
 }
 
-export class Plugin<C = any> implements PluginFactory {
+export class Plugin implements PluginFactory {
   public static runPluginsMethod(
     plugins: Plugin[] | undefined
   ): (method: keyof Plugin, ...args: any[]) => void {
@@ -88,26 +72,16 @@ export class Plugin<C = any> implements PluginFactory {
   }
 
   public readonly setConfig?: SetConfig;
-  public readonly onCreateWebpackConfig?: OnCreateWebpackConfig<C>;
-  public readonly onCreateBabelConfig?: OnCreateBabelConfig;
   public readonly modifyFiles?: ModifyFiles;
   public readonly modifyEntry?: ModifyEntry;
-  public readonly onCreateDevServer?: OnCreateDevServer;
-  public readonly onPreBuild?: OnPreBuild;
-  public readonly onPostBuild?: OnPostBuild;
 
   constructor(p: PluginFactory) {
     this.setConfig = p.setConfig;
-    this.onCreateWebpackConfig = p.onCreateWebpackConfig;
-    this.onCreateBabelConfig = p.onCreateBabelConfig;
     this.modifyFiles = p.modifyFiles;
     this.modifyEntry = p.modifyEntry;
-    this.onCreateDevServer = p.onCreateDevServer;
-    this.onPreBuild = p.onPreBuild;
-    this.onPostBuild = p.onPostBuild;
   }
 }
 
-export function createPlugin<C = any>(factory: PluginFactory): Plugin<C> {
+export function createPlugin(factory: PluginFactory) {
   return new Plugin(factory);
 }
